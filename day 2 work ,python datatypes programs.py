028f95d2b84d3cef3d5687514d3a275b06bba85e
Dhruv Darji <84891788+DhruvDarji11@users.noreply.github.com>
2021-05-30 16:35:49 +0530

    Add files via upload

---
+++ day 2 work ,python datatypes programs.py
@@ -0,0 +1,83 @@
+# 1st example of comment
+
+# 1st here # is use for single line comment
+
+# 2nd """...."""  3 time single or double quoteds is used for multi line comment
+
+# declaring variables  ....................1
+
+'''a,b,c=2,3.4,"dhruv"
+d=e=f=10
+
+a=3
+b=29
+
+print(a)
+print(b)
+print(c)'''
+
+
+# From here data types of python and its example starts
+
+# NUMBER DATATYPE ....................................................2
+
+'''a=23
+b=32.56
+c="dhruv"
+print("data type of",a, "is ",type(a))
+print("data type of",b, "is ",type(b))
+print("data type of",c, "is ",type(c))'''
+
+#STRING DATATYPE.......................................................3
+"""
+name="Dhruv Darji"
+
+print("name is ",name,"type is",type(name))
+
+# operation on string like slice dice
+
+print(name[3])
+print(name[2:5])
+print(name[:8])
+print(name[4:])
+print(name +" Bharatbhai")"""
+
+#  LIST DATATYPE........................................................4
+
+"""lst=[43,23,"dhruv",12]
+
+print(lst)
+
+#operation on list
+
+print(lst[3])
+lst.append(10)
+print(lst)
+print(lst[1:3])"""
+
+
+#TUPLE  DATATYPE....................................................................5
+
+#tuple cannot be modified but list can be
+
+"""tup1=(1,"dhruv",11,32.23,"darji")
+print(tup1)
+
+#operation
+
+print(tup1[3])
+print(tup1[0:3])
+print(tup1[5])"""
+
+#DATA DICTIONARY.........................................................6
+
+dict={10:"dhruv",20:"datji","simran":30,"Raj":40}
+print(dict)
+print(type(dict))
+
+#OPERATION
+
+
+print(dict[10])
+print(dict[20])
+print(dict["simran"])
